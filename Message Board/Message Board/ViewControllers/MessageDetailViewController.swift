1810e407fea8abfafea069682aa131ce3e07a922
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-08-08 15:25:01 -0700

    Implements initial storyboard layout.

---
+++ Message Board/Message Board/ViewControllers/MessageDetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  MessageDetailViewController.swift
+//  Message Board
+//
+//  Created by Jonathan T. Miles on 8/8/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class MessageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func Send(_ sender: Any) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - Properties
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var messsageTextField: UITextView!
+    
+
+}
